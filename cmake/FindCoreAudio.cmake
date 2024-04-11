9eb505f9f2ad79c739f1dafdcd3700d1a897cbb9
cedric <cedric.paille@gmail.com>
2024-04-11 18:00:20 +0200

    Fix cmake find packages

--- cmake/FindCoreAudio.cmake
+++ cmake/FindCoreAudio.cmake
@@ -11,6 +11,6 @@ find_path(COREAUDIO_INCLUDE_DIR NAMES CoreAudio/CoreAudio.h)
 find_library(COREAUDIO_LIBRARY NAMES CoreAudio)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(COREAUDIO DEFAULT_MSG COREAUDIO_LIBRARY COREAUDIO_INCLUDE_DIR)
+find_package_handle_standard_args(CoreAudio DEFAULT_MSG COREAUDIO_LIBRARY COREAUDIO_INCLUDE_DIR)
 
 mark_as_advanced(COREAUDIO_INCLUDE_DIR COREAUDIO_LIBRARY)
